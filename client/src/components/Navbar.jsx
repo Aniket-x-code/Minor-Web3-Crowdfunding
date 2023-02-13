a95da6e7602fbd504a81f7e1818e4162fa6c48c0
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-02-13 22:01:48 +0530

    adding hooks

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,17 @@
-import React, {useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { CusttomButton} from './CustomButton';
+import { logo, menu, search,thirdweb } from '../assets';
+import { navlinks } from '../constants';
+
 const Navbar = () => {
+const navigate = useNavigate();
+const [ isActive , setisActive] = useState('dashboard');
+
+
+
+
+
   return (
     <div>Navbar Test </div>
   )
