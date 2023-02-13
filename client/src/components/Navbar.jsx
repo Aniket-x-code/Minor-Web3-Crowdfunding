aa53f4c774a623ae0a27f5a3ff0633d524d4cd81
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-02-13 22:44:17 +0530

    added search bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,29 @@
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { CusttomButton} from './CustomButton';
+import { CustomButton} from './'
 import { logo, menu, search,thirdweb } from '../assets';
 import { navlinks } from '../constants';
 
 const Navbar = () => {
 const navigate = useNavigate();
+
 const [ isActive , setisActive] = useState('dashboard');
 
-
-
-
+const [ toggleDrawer, settoggleDrawer]= useState(false);
 
   return (
-    <div>Navbar Test </div>
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] ap-6">
+      <div className="lg:flex-1 flex  flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+
+      <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
+
+      <div className="w-[52px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
+
+        <img src={search} alt='search' className="w-[15px] h-[15px] object-contain"/>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
