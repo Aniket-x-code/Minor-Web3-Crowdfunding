c0615dbefd523883c43fb76308810ee786054f5a
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2023-04-17 23:39:11 +0530

    adding details

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { useStateContext } from '../context';
+import {  CustomButton } from '../components';
+import { calculateBarPercentage, daysLeft } from '../utils';
+import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
+  const { state } = useLocation();
+  const {  getDonations } = useStateContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [donators, setDonators] = useState([]);
+  const remainingDays = daysLeft(state.deadline);
+
+
+
   return (
-    <div>CampaignDetails</div>
+    <div>
+      {isLoading && 'Loading..'}
+
+      
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img src={state.image} alt="campaign" className="w-590px h-[410px] object-contain rounded-xl"/>
+          <div className="relative w-full h-[5px] bg-[#7321b1] mt-2">
+            <div className="absolute h-full bg-[#fdfffe]" style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
+            </div>
+          </div>
+        </div>
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox title={`Raised of ${state.target}`} value={state.amountCollected} />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
